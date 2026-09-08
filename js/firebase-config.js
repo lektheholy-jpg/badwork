837785ef4e1357c026755765adc1d64427c9107f
np-school <lektheholy@gmail.com>
2026-09-08 18:04:00 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@
 // หาได้จาก Firebase Console > Project settings > General > Your apps > SDK setup
 // ==========================================================================
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyANOCrtJ8lfNj_yLFsEMhRwFmzPgoyLSiE",
+  authDomain: "mywork-lektheholy.firebaseapp.com",
+  projectId: "mywork-lektheholy",
+  storageBucket: "mywork-lektheholy.firebasestorage.app",
+  messagingSenderId: "556231214049",
+  appId: "1:556231214049:web:90a10308d23f022dcdc8a0",
 };
 
 firebase.initializeApp(firebaseConfig);
